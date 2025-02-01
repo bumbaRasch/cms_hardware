d29565721474d05684fa9ee28ac64ef271e93a09
bumbaRasch <pisarevdimitri@gmail.com>
2025-02-01 21:25:13 +0100

    Add Fastify CORS and EJS view engine support in the backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,34 @@
-import Fastify from 'fastify'
-import routes from './routes/index.js'
+import Fastify from 'fastify';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fastifyView from '@fastify/view';
+import ejs from 'ejs';
+import routes from './routes/index.js';
+import fastifyCors from '@fastify/cors';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const fastify = Fastify({
   logger: true
-})
+});
+
+fastify.register(fastifyView, {
+  engine: {
+    ejs: ejs
+  },
+  root: path.join(__dirname, 'views'),
+  viewExt: 'ejs'
+});
+
+fastify.register(fastifyCors, {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+});
 
 fastify.register(routes);
+
 fastify.listen({ host: '0.0.0.0', port: 3000 }, (err, address) => {
-    if (err) throw err
-    fastify.log.info(`Server listening on ${address}`)
-})
+  if (err) throw err;
+  fastify.log.info(`Server listening on ${address}`);
+});
