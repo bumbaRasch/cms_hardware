9b1aa15403d94a4bb2fe0adbf4c0f88361b322db
bumbaRasch <pisarevdimitri@gmail.com>
2025-02-05 17:18:30 +0100

    Remove time formatting from date utility function to simplify date output

--- backend/src/utils/date.js
+++ backend/src/utils/date.js
@@ -4,8 +4,8 @@ export const formatDate = (date) => {
         day: '2-digit',
         month: '2-digit',
         year: 'numeric',
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit'
+        // hour: '2-digit',
+        // minute: '2-digit',
+        // second: '2-digit'
     }).format(new Date(date));
 };
