2247aa2d890060d41e99cd164de274fe11a0c8af
bumbaRasch <pisarevdimitri@gmail.com>
2025-02-09 21:27:55 +0100

    Refactor types table initialization and enhance delete functionality with improved event handling

--- backend/public/js/types.js
+++ backend/public/js/types.js
@@ -1,15 +1,76 @@
+$(document).ready(function() {
+    const table = $('#table');
+
+    table.bootstrapTable({
+        columns: [{
+            checkbox: true,
+            visible: true
+        }, {
+            field: 'HT_NAME',
+            title: 'Name',
+            sortable: true
+        }, {
+            field: 'HT_CATEGORY',
+            title: 'Category',
+            sortable: true
+        }, {
+            field: 'HT_DESCRIPTION',
+            title: 'Description',
+            sortable: true
+        }, {
+            field: 'operate',
+            title: 'Actions',
+            align: 'center',
+            clickToSelect: false,
+            events: window.operateEvents,
+            formatter: operateFormatter
+        }],
+        classes: 'table table-bordered table-striped table-hover',
+        clickToSelect: true,
+        filterControl: true,
+        idField: 'HT_ID',
+        locale: 'en-US',
+        pageList: [10, 25, 50, 'all'],
+        pagination: true,
+        search: true,
+        searchAlign: 'left',
+        showColumns: true,
+        showColumnsToggleAll: true,
+        showExport: true,
+        showFooter: true,
+        showFullscreen: true,
+        showPaginationSwitch: true,
+        showToggle: true,
+        sidePagination: 'server',
+        sortable: true,
+        theadClasses: 'table-primary',
+        toggle: true,
+        toolbar: '#toolbar',
+        url: '/api/types',
+    });
+
+    function operateFormatter(value, row, index) {
+        return `
+            <div class="d-grid gap-2 d-md-flex justify-content-md-center">
+                <button class="btn btn-sm btn-warning edit" title="Edit"><i class="bi bi-pencil"></i></button>
+                <button class="btn btn-sm btn-danger delete" title="Delete"><i class="bi bi-trash"></i></button>
+            </div>
+        `;
+    } 
+});
+
+
 window.operateEvents = {
     'click .delete': async function (e, value, row, index) {
         if (confirm('Are you sure you want to delete this item?')) {
             try {
                 const response = await fetch(`/api/types/${row.HT_ID}`, {
-                    method: 'DELETE'
+                    method: 'DELETE',
                 });
 
-                console.log(response);
-
                 if (response.ok) {
-                    $('#table').bootstrapTable('remove', {
+                    const table = $('#table');
+                    table.bootstrapTable('remove', {
                         field: 'HT_ID',
                         values: [row.HT_ID]
                     });
@@ -24,11 +85,3 @@ window.operateEvents = {
     }
 };
 
-function operateFormatter(value, row, index) {
-    return `
-        <div class="d-grid gap-2 d-md-flex justify-content-md-center">
-            <button class="btn btn-sm btn-warning edit" title="Edit"><i class="bi bi-pencil"></i></button>
-            <button class="btn btn-sm btn-danger delete" title="Delete"><i class="bi bi-trash"></i></button>
-        </div>
-    `;
-}
